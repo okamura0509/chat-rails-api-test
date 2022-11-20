7079de667a2317c0240abd209653864ed415f73f
okamura0509 <milktea.chaba2bay@gmail.com>
2022-11-21 02:53:57 +0900

    ログイン認証機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  namespace :api do
     namespace :v1 do
-      resources :test, only: %i[index]
+      mount_devise_token_auth_for 'User', at: 'auth'
+
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
     end
-  end
 end
